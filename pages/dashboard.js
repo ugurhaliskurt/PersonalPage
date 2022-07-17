599eb4215384b8b7796d9e16bf2c136fbdf8b773
ugurkurt <ugurkurt90@gmail.com>
2022-07-17 22:22:32 +0300

    first Completed one

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -13,7 +13,17 @@ function sendRequest()
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == XMLHttpRequest.DONE) {
-            console.log(JSON.stringify(JSON.parse(xmlHttp.responseText)));
+            jsonArray = JSON.parse(xmlHttp.responseText);
+            var dates = [];
+            var values = [];
+            var payments = [];
+            for(var k in jsonArray) {
+                dates.push(jsonArray[k]["Date"]);
+                values.push(jsonArray[k]["Value"]);
+                payments.push(jsonArray[k]["Paid"]);
+            }
+            drawChart(dates, values, payments);
+            console.log(payments);
         }
     }
     xmlHttp.open( "POST", 'http://127.0.0.1:34568', true ); // false for synchronous request
@@ -44,4 +54,46 @@ function sendRequest()
     console.log(JSON.stringify(data));
     xmlHttp.send(JSON.stringify(data) );
     return xmlHttp.responseText;
+}
+
+var myChart
+function drawChart( Dates, Values, Paymets )
+{
+    if (myChart !== undefined)
+        myChart.destroy();
+    var ctx = document.getElementById("myChart");
+    myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: Dates,
+        datasets: [{
+          data: Values,
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: "green",
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        },
+        {
+          data: Paymets,
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: "red",
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false,
+        }
+      }
+    });
 }
